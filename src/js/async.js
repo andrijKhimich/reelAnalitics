d0883765441b6f596d2e5ae32c28a9de8268233c
Andrew Khimich <andrijkhimich@gmail.com>
2023-05-26 09:26:16 +0300

    code format

--- src/js/async.js
+++ src/js/async.js
@@ -1,18 +1,17 @@
-const jsonFile = document.querySelector('.json-file');
+const jsonFile = document.querySelector(".json-file");
 
 if (jsonFile) {
-  const url = jsonFile.textContent;
+	const url = jsonFile.textContent;
 
-  const getData = async () => {
-    fetch(url)
-      .then((response) => response.json())
-      .then((json) => {
-        // console.log(json.length)
-        // return json;
-        for (let i = 0; i < json.length; i++) {
-          // console.log(json[i]);
-          let template =
-            `<tr>
+	const getData = async () => {
+		fetch(url)
+			.then(response => response.json())
+			.then(json => {
+				// console.log(json.length)
+				// return json;
+				for (let i = 0; i < json.length; i++) {
+					// console.log(json[i]);
+					let template = `<tr>
           <td>
             <div class="table-user">
               <div class="table-name">
@@ -49,19 +48,19 @@ if (jsonFile) {
             <span>${json[i].Class}</span>
           </td>
         </tr>`;
-          // console.log(template);
-          // console.log();
-          const table = document.querySelector('.board-scroll table');
-          // console.log(tableElement);
-          table.insertAdjacentHTML('beforeEnd', template);
-        }
-      })
+					// console.log(template);
+					// console.log();
+					const table = document.querySelector(".board-scroll table");
+					// console.log(tableElement);
+					table.insertAdjacentHTML("beforeEnd", template);
+				}
+			});
 
-    // const url = './data/data.json';
-    // const response = await fetch(url);
-    // const data = await response.json();
-    // console.log(data);
-    // return data;
-  };
-  getData();
+		// const url = './data/data.json';
+		// const response = await fetch(url);
+		// const data = await response.json();
+		// console.log(data);
+		// return data;
+	};
+	getData();
 }
